4839094579d7d75adc5f80aae98b2997804d7609
bangdler <zbthz90@gmail.com>
2023-01-19 17:49:59 +0900

    강의 generic

--- example/8_generics.ts
+++ example/8_generics.ts
@@ -1,3 +1,4 @@
+// 제네릭을 사용하지 않으면 함수에 매개변수와 반환값의 타입을 정해야 하고, 이는 중복된 함수를 만들게 된다.
 function getNumber(value: number) {
   return value;
 }
@@ -6,6 +7,14 @@ function getArray(value: string[]) {
   return value;
 }
 
+// 이를 해결하기 위해 union 을 활용할 수도 있지만 반환값 또한 union 타입이라 api 가 제한되게 된다.
+function getNumArr(value: string | string[]) {
+  return value;
+}
+const a = getNumArr('100');
+// a.split(); // a 의 type 은 string | string[] 이므로 split 사용 시 에러
+
+// generic 을 통해 타입을 미리 정하지 않고 나중에 타입 추론이 가능하게 된다.
 // 제네릭 기본 문법 - 함수
 function getValue<T>(value: T): T {
   return value;
@@ -26,12 +35,27 @@ function getNumberAndArray<T>(value: T): T {
   return value;
 }
 
-function getNumberAndArray<T>(value: T[]): T[] {
+function getNumberAndArray2<T>(value: T[]): T[] {
   value.length; // O
   return value;
 }
 
+// 제네릭의 속성에 대한 타입을 미리 정의할 수 있다.
+interface LengthType {
+  length: number;
+}
+
+function getNumberAndArray3<T extends LengthType>(value: T): T {
+  value.length;
+  return value;
+}
+
+getNumberAndArray3(10); // length 속성이 없다.
+getNumberAndArray3({ length: 10 });
+getNumberAndArray3('10');
+
 // 제네릭 타입 제한 - keyof
+// 상속받는 interface 의 key 값만 들어올 수 있다.
 interface ShoppingItems {
   name: string;
   price: number;
@@ -49,5 +73,7 @@ function getAllowedOptions<T extends keyof ShoppingItems>(option: T): T {
   }
 }
 getAllowedOptions('nothing');
+getAllowedOptions('name');
+getAllowedOptions({ name: 'name' });
 // const a = getAllowedOptions('name');
 // a.toUpperCase(); // Name
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,41 @@
-const emails = [
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+interface ProductNum {
+  value: number;
+  selected: boolean;
+}
+
+// Email, ProductNum 대신 제네릭 사용하면 중복을 줄일 수 있다.
+const emails: Dropdown<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: Dropdown<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+// Email, ProductNum 타입을 모두 사용하는 함수
+// function createDropdownItem(item: Dropdown<string> | Dropdown<number>) {
+//   const option = document.createElement('option');
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+// unoin 사용하지 않도록 업데이트
+function createDropdownItem<T>(item: Dropdown<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -20,7 +45,7 @@ function createDropdownItem(item) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<typeof email.value>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
