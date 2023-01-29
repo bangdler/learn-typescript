2ee88b6917f2ccfe869ab9448132fc671b03316b
bangdler <zbthz90@gmail.com>
2023-01-29 16:19:25 +0900

    강의 타입추론

--- example/9_type-inference.ts
+++ example/9_type-inference.ts
@@ -1,26 +1,27 @@
 var a = 'a';
 
+// 문자열 + 숫자 -> 문자열로 타입 추론
 function logA(a = 'a') {
   var b = 10;
-  return b;
+  return a + b;
 }
 
 interface Dropdown<T> {
-  value: T
+  value: T;
   title: string;
 }
 var items: Dropdown<number> = {
   value: 10,
-  title: 'a'
-}
+  title: 'a',
+};
 
 interface DetailedDropdown<T> extends Dropdown<T> {
   description: string;
   tag: T;
 }
-var detailItems: DetailedDropdown<number> = {
-  value: 'hi',
+var detailItems: DetailedDropdown<string> = {
+  value: '10',
   title: 'a',
   description: 'b',
-  tag: 'c'
-}
+  tag: 'c',
+};
